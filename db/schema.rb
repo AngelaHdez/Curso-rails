9340ffdc5ec10fbf588e09ad4f003278c57c71a7
root <root@curso-VirtualBox.(none)>
2013-05-18 10:10:14 +0100

    migraciones añadimos tabla

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130511120535) do
+ActiveRecord::Schema.define(:version => 20130518084417) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130511120535) do
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "published"
   end
 
 end
